89862c80d4f87ec2ccbddc185bfaa85a16afc078
seokhyeon-Park <dfbgq346q@gmail.com>
2024-01-24 11:37:38 +0900

    기본 디자인 샘플

--- src/page/Main.js
+++ src/page/Main.js
@@ -1,12 +1,14 @@
 import style from '../style/page/Main.module.css';
 import LoginContainer from '../component/LoginContainer';
 import AppListConatiner from '../component/AppListContainer';
+import SearchContainer from '../component/SearchContainer';
 
 function Main() {
   return (
     <div className={style.Main}>
-      {/* <LoginContainer /> */}
-      <AppListConatiner />
+      <LoginContainer />
+      {/* <AppListConatiner /> */}
+      {/* <SearchContainer /> */}
     </div>
   );
 }
