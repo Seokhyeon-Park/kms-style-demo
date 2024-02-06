8552fd8673c4119dd6d23f2c82c66cf80c31c57e
seokhyeon-Park <dfbgq346q@gmail.com>
2024-02-06 18:03:36 +0900

    *

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import './style/index.css'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <React.StrictMode>
     <div style={{ height: '100%', width: '100%' }}>
